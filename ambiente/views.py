db64a6a1a5c1fa924612a8a8321dad36a8a32b77
pauloabaia <paulinhus2@outlook.com>
2025-11-24 14:21:58 -0300

    feat: padronização de templates & listagem de ambientes com count de atividades

--- ambiente/views.py
+++ ambiente/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render, redirect
 
 from ambiente.forms import AmbienteForm
 from ambiente.models import Ambiente
+from django.db.models import Count
 
 # Create your views here.
 
 class AmbienteView:
     def lista_ambientes(request):
-        ambientes = Ambiente.objects.all()
+        ambientes = Ambiente.objects.annotate(num_atividades=Count('atividade'))
         return render(request, 'ambiente/home.html', {'ambientes': ambientes})
     
     def detalhe_ambiente(request, ambiente_id):
-        return render(request, 'ambiente/detalhe.html', {'ambiente_id': ambiente_id})
+        ambiente = Ambiente.objects.get(id=ambiente_id)
+        atividades = ambiente.atividade_set.all()
+        return render(request, 'ambiente/detalhe.html', {
+            'ambiente': ambiente,
+            'atividades': atividades
+        })
     
     def criar_ambiente(request):
         if request.method == 'POST':
