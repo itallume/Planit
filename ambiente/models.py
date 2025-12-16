65e89f9a61d6b41bd8f3fd1b581b48e32031d61a
pauloabaia <paulinhus2@outlook.com>
2025-12-16 17:20:00 -0300

    feat: campo tema em ambiente se torna opcional

--- ambiente/models.py
+++ ambiente/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Ambiente(models.Model):
 
     nome = models.CharField(max_length=100)
-    tema = models.CharField(max_length=100)
+    tema = models.CharField(max_length=100, blank=True, null=True)
     usuario_administrador = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     usuarios_participantes = models.ManyToManyField('auth.User', related_name='ambientes_participantes', blank=True)
 
