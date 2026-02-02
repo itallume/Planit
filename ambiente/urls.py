512c855ebecd8d0ae85c74119e6a8ba5ed89f66d
pauloabaia <paulinhus2@outlook.com>
2026-02-02 14:50:49 -0300

    feat: corrigir rotas de convite e ações de aceitar/recusar no base.html

--- ambiente/urls.py
+++ ambiente/urls.py
@@ -5,6 +5,14 @@ invitation_create = AmbienteInvitationViewSet.as_view({
     'post': 'create'
 })
 
+invitation_accept = AmbienteInvitationViewSet.as_view({
+    'post': 'accept'
+})
+
+invitation_decline = AmbienteInvitationViewSet.as_view({
+    'post': 'decline'
+})
+
 urlpatterns = [
     path('', AmbienteView.lista_ambientes, name='lista_ambientes'),
     path('criar/', AmbienteView.criar_ambiente, name='criar_ambiente'),
@@ -12,4 +20,6 @@ urlpatterns = [
     path('<int:ambiente_id>/deletar/', AmbienteView.deletar_ambiente, name='deletar_ambiente'),
     path('<int:ambiente_id>/convidar/', invitation_create, name='enviar_convite'),
     path('<int:ambiente_id>/configurar/', AmbienteView.configurar_ambiente, name='configurar_ambiente'),
+    path('convite/<int:pk>/aceitar/', invitation_accept, name='aceitar_convite'),
+    path('convite/<int:pk>/recusar/', invitation_decline, name='recusar_convite'),
 ]
